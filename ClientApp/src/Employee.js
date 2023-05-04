65d966779789b45c45eed15f2b2492d26bdb23cc
djking <djking@Second-Test.myguest.virtualbox.org>
2023-05-04 16:49:08 -0500

    Added JS Libraries, Removed Bloat Files

---
+++ ClientApp/src/Employee.js
@@ -0,0 +1,10 @@
+import "./components/Employee.css";
+
+function Employee(){
+    return <div className="Employee">
+        <label>Name:</label><br/>
+        <input type="text" />
+    </div>
+}
+
+export default Employee;
