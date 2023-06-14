b201ac854a3e0417a98fb03d464f96658f91e475
djking <djking@Second-Test.myguest.virtualbox.org>
2023-06-14 16:01:48 -0500

    Added mdbootstrap, Added Footer, Organized Files

---
+++ ClientApp/src/components/layout/footer/Footer.js
@@ -0,0 +1,13 @@
+import React,{ Component } from "react";
+import {  } from 'reactstrap';
+import './Footer.css';
+
+export class Footer extends Component{
+    render() {
+        return (
+            <footer>
+                <p>Practice Website @ 2023</p>
+            </footer>
+        );
+    }
+}
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, parsePath } from 'react-router-dom';
 import App from './App';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
