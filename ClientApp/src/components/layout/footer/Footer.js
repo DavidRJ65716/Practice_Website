1110583b8bb093ad447410aba846006a9e68a72d
djking <djking@Second-Test.myguest.virtualbox.org>
2023-07-14 16:37:19 -0500

    Connect MySQL to backend

--- ClientApp/src/components/layout/footer/Footer.js
+++ ClientApp/src/components/layout/footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 export class Footer extends Component{
     render() {
         return (
-            <MDBFooter bgColor="light" className="text-center text-lg-start text-muted" >
+            <MDBFooter bgColor="light" className="text-center p-4 text-lg-start text-muted" >
                 <div className="text-center" style={{ backgroundColor: 'rgba(0,0,0,0.05)'}}>
                     Practice Website @ 2023 No Copyright
                 </div>
---
+++ Models/Employees.cs
@@ -0,0 +1,12 @@
+namespace projects.Models{
+
+    public class Employees{
+
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Department {get; set;}
+        public int PhoneNumber {get; set;}
+        public string Email {get; set;}    
+    }
+}
